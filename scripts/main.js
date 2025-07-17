acee5253d0e39b034dadb1774d6dbdcd657a0378
Akssses <itshnik07@gmail.com>
2025-07-17 22:58:00 +0600

    home page

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const loadComponent = async (name) => {
+  const res = await fetch(`../components/${name}/${name}.html`);
+  const html = await res.text();
+  document.querySelector(`[data-component="${name}"]`).innerHTML = html;
+
+  // Подключение CSS
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = `../components/${name}/${name}.css`;
+  document.head.appendChild(link);
+
+  // Подключение JS
+  const script = document.createElement("script");
+  script.src = `../components/${name}/${name}.js`;
+  document.body.appendChild(script);
+};
+
+["header", "statistics", "navbar"].forEach(loadComponent);
